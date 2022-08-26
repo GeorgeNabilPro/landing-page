616b7e696b44493ee5589c84c615a8b2a3bec549
George Nabil <georgenabilpro@gmail.com>
2022-08-26 13:06:23 +0200

    resizing bug fixed, more comments added

--- js/app.js
+++ js/app.js
@@ -21,7 +21,9 @@ for (let i=0; i<secN; i++) {
 
     const li = document.createElement('li');
     li.textContent = sectionName;
+    // giving each item in the nav menu an ID so it can be accessed properly
     li.id = `list__item--${i}`
+    // adding an event listener so that when the li is clicked the window is smoothly scrolled to the specified section
     li.addEventListener('click',function(ev){
         smoothScroll(allSections[i])
     })
@@ -32,15 +34,21 @@ for (let i=0; i<secN; i++) {
 
 navList.firstChild.classList.add("active")
 
-function activate(sectionId){
+function activate(sectionRef,section){
+    // activating the section in the nav menu
     for (let i=0; i<navList.childElementCount;i++) {
-        let section = navList.children[i];
-        if (section.id === sectionId){
-            section.classList.add("active")
+        let sectionInNav = navList.children[i];
+        if (sectionInNav.id === sectionRef){
+            sectionInNav.classList.add("active")
         } else {
-            section.classList.remove("active")
+            sectionInNav.classList.remove("active")
         }
     }
+    // activating the section in the body
+    for (let sec of allSections){
+        sec.classList.remove('active')
+    }
+    section.classList.add('active')
 }
 
 document.addEventListener('scroll',function checkAndActivate(evt){
@@ -48,7 +56,7 @@ document.addEventListener('scroll',function checkAndActivate(evt){
         let secRef = navList.children[i]
         let section = allSections[i]
         if ((section.getBoundingClientRect().top <= 350) && (section.getBoundingClientRect().top > 0)) {
-            activate(secRef.id)
+            activate(secRef.id, section)
         }
     }
 })
@@ -74,3 +82,12 @@ navBtn.addEventListener('click',function (evt) {
         navBtn.innerHTML = `<i class="fa-solid fa-bars"></i> topics`
     }
 })
+
+// fixing the resizing bug 
+window.onresize = (evt) => {
+    if (window.innerWidth > 850 && navBtn.classList.contains('clicked')) {
+        header.classList.remove("aside")
+        nav.classList.remove("aside")
+        navBtn.innerHTML = `<i class="fa-solid fa-bars"></i> topics`
+    }
+}
